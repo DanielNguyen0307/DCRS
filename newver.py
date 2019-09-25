74f6b5145dd21be0337da8b65b24367b074b2a92
DanielNguyen <danielnguyen.na@gmail.com>
2019-09-25 16:24:37 +0700

    Ver3

--- newver.py
+++ newver.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 import cx_Oracle
 import matplotlib.pyplot as plt
 import numpy as np
@@ -46,22 +49,55 @@ amountLableTP.reverse()
 dateLable.reverse()
 
 ## Processing Data And Draw
-## Refer link: https://matplotlib.org/3.1.1/gallery/lines_bars_and_markers/bar_stacked.html#sphx-glr-gallery-lines-bars-and-markers-bar-stacked-py
-N = 7
 
+data = [amountLableTP,amountLableCD]
+columns = dateLable
+rows = ['Topup', 'Card']
+
+#values = np.arange(0, 25000, 5000)
+#value_increment = 1000
+
+# Get some pastel shades for the colors
+colors = plt.cm.BuPu(np.linspace(0, 0.5, len(rows)))
+n_rows = len(data)
+
+index = np.arange(len(columns)) + .3
+
+# Initialize the vertical-offset for the stacked bar chart.
+y_offset = np.zeros(len(columns))
+
+# Plot bars and create text labels for the table
+cell_text = []
+for row in range(n_rows):
+    y_offset = data[row]
+    cell_text.append(['%1.1f' % (x / 1000.0) for x in y_offset])
+# Reverse colors and text labels to display the last value at the top.
+colors = colors[::-1]
+#cell_text.reverse()
+
+# Add a table at the bottom of the axes
+the_table = plt.table(cellText=cell_text,
+                      rowLabels=rows,
+                      rowColours=colors,
+                      colLabels=columns,
+                      loc='bottom')
+
+# Adjust layout to make room for the table:
+plt.subplots_adjust(left=0.2, bottom=0.2)
+
+values = np.arange(0, 475000, 50000)
+value_increment = 1000
+N = len(columns)
 ind = np.arange(N)
-width = 0.35
+width = 0.4
+
 p1 = plt.bar(ind, amountLableTP, width)
 p2 = plt.bar(ind, amountLableCD, width, bottom=amountLableTP)
-
-plt.ylabel('Daily Total Amount')
-plt.title('DCRS Sale Amount Report Weekly')
-plt.xticks(ind, dateLable)
-plt.yticks(np.arange(0, 400000000, 25000000))
-
+plt.xticks([])
+plt.ylabel('Daily Total Amount (Thousand VND)')
+plt.yticks(values * value_increment, ['%d' % val for val in values])
+plt.title('DCRS\'s Sale Amount Report Weekly')
 plt.legend((p1[0], p2[0]), ('TopUp', 'Card'))
+plt.grid(b=True, which='major', color='#666666', linestyle='-')
 
 plt.show()
-
-
-
